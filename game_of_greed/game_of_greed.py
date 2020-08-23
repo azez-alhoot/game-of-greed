51de45460abc03df4b86437a899c5fb16549ca95
Osama-Yousef <osamawalidyousef@gmail.com>
2020-08-23 19:01:48 +0300

    sol

--- game_of_greed/game_of_greed.py
+++ game_of_greed/game_of_greed.py
@@ -97,18 +97,267 @@
 
 
 
-import random
 
-@staticmethod
-def role_dice(tup):
-  output = []
 
-  for i in range(len(tup)):
-      output.append(random.randint(1,6))
 
-  return tuple(output)
-  
-print(role_dice((1,5,6,5)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+# import random
+
+# class GameOfGreed():
+#         def __init__(self):
+#             pass
+
+#         @staticmethod
+        
+#         def calculate_score(self, dice):
+#           dice = Counter(dice)
+#           score = 0
+#         # test for straight
+
+#           if 1 in dice and 2 in dice and 3 in dice and 4 in dice and 5 in dice and 6 in dice:
+#               score += 1500
+#               return score
+              
+#           # test for 3 pairs
+#           if len(dice) == 3:
+                              
+#               return 750
+
+#           for die, count in dice.items():
+              
+#               if die < 3 and count == 1:
+#                 score += (die * 100)
+
+
+
+#               if die == 1:
+#                   if count >= 3:
+#                       score += (count - 2) * 1000
+#                   else:
+#                       score += count * 100
+#               if die == 2:
+#                   if count >= 3:
+#                       score += (count - 2) * 200
+#               if die == 3:
+#                   if count >= 3:
+#                       score += (count - 2) * 300
+#               if die == 4:
+#                   if count >= 3:
+#                       score += (count - 2) * 400
+#               if die == 5:
+#                   if count >= 3:
+#                       score += (count - 2) * 500
+#                   else:
+#                       score += count * 50
+#               if die == 6:
+#                   if count >= 3:
+#                       score += (count - 2) * 600
+                  
+#           return score
+
+# print(GameOfGreed.calculate_score(1,2,3,4,5,6))
+
+
+
+
 
 
 
